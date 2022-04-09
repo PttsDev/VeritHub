bce3f061c0ed87498909d9ca5fa6b7090e037460
Roberto <47633569+Ptts2@users.noreply.github.com>
2022-04-09 13:34:05 +0200

    Hotfix

--- client/verithub-front/src/main.js
+++ client/verithub-front/src/main.js
@@ -3,4 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+
+Vue.prototype.$server =  'http://127.0.0.1:3000';
 createApp(App).use(store).use(router).mount('#app')
