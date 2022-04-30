e793d46c7f2217d721c33f4e5a80ec8b1a42ac18
agonzg40 <agonzg40@estudiantes.unileon.es>
2022-04-30 18:15:26 +0200

    Hecho el titulo y algun ajuste

---
+++ client/verithub-front/src/components/SearchBar.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>{{title}}</h1>
+        <v-textarea
+          name="searchUniversity"
+          no-resize
+          clearable
+          clear-icon="mdi-close-circle"
+        ></v-textarea>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+    data: () => ({
+        title: 'VeritHub',
+    }),
+}
+
+</script>
+
+<style lang="css" scoped>
+    h1{
+        position: relative;
+        text-align: center;
+        top: 150px;
+        font-size: 50px;
+        font-weight: 100;
+        color: gray;
+    }  
+
+    .v-textarea{
+        position: relative;
+        text-align: center;
+        padding-left: 300px;
+        top: 300px;
+        font-size: 50px;
+        width: 80%;
+    }
+</style>
