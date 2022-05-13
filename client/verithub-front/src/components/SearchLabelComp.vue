dfe65d2fca13bbd4e6c051cbe371793723ed749b
AlvaroCasado99 <47740698+AlvaroCasado99@users.noreply.github.com>
2022-05-13 16:04:43 +0200

    Algunos estilos creados, estructura básica inicial

--- client/verithub-front/src/components/SearchLabelComp.vue
+++ client/verithub-front/src/components/SearchLabelComp.vue
@@ -10,7 +10,18 @@ Partes:
 
 <template>
     <div id="initial">
-        
+        <div id="show-images">
+            <img class="center-img" src="./1.jpg">
+            
+        </div>
+        <div id="show-info">
+            <!--Contiene:
+                Título, Distancia, Estrellas, Opiniones.-->
+
+            <h2 class="label-title">Nombre de la Universidad</h2>
+            <v-rating class="star-rating" value="3" hover color="warning" ></v-rating>
+        </div>
+
     </div>
 </template>
 
@@ -18,4 +29,26 @@ Partes:
 </script>
 
 <style>
+    #initial {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        width: 50%;
+        height: 25%;
+    }
+    #show-images {
+        width: 33%;
+    }
+    #show-info {
+        width: 66%;
+        display: flex;
+        flex-direction: column; /*para stackear los elementos*/
+    }
+    .center-img{
+        width: 100%;
+        height: 100%;
+    }
+    
+
 </style>
--- client/verithub-front/src/views/ResultsView.vue
+++ client/verithub-front/src/views/ResultsView.vue
@@ -1,10 +1,24 @@
 <template>
-  <div>
+  <!--Clave API: AIzaSyD_IlgiAW6kpxobip8z7gW6lcPoJsK9yHA-->
+  <div id="initial">
     <HeaderComp />
-    <SearchBar />
+    <div id="main-body-conteiner">
+      <div id="search-bar-container">
+        <!--Aquí vendríala search bar-->
+      </div>
+      <div id="content-container">
+        <div id="results-container">
+          <!--Aquí viene la lista de resultados-->
+          <SearchLabelComp />
+          <SearchLabelComp />
+          <SearchLabelComp />
+        </div>
+        <div id="map-container">
+          <!--Aquí viene el mapa-->
+        </div>
+      </div>
+    </div>
     <FooterComp />
-    <SearchLabelComp />
-    
   </div>
 </template>
 
@@ -13,7 +27,6 @@ import { defineComponent } from 'vue';
 
 // Components
 import FooterComp from '../components/FooterComp.vue';
-import SearchBar from '../components/SearchBar.vue';
 import HeaderComp from '../components/HeaderComp.vue';
 import SearchLabelComp from '../components/SearchLabelComp.vue';
 
@@ -22,7 +35,6 @@ export default defineComponent({
 
   components: {
     FooterComp,
-    SearchBar,
     HeaderComp,
     SearchLabelComp,
   },
@@ -43,6 +55,50 @@ export default defineComponent({
   /* Todo el texto en esa fuente */
   body {
     font-family: 'Palatino Linotype', 'Times New Roman', serif;
+    margin:0;
+    padding:0;
+  }
+
+  #initial {
+    display: flex;
+  }
+
+  #main-body-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #search-bar-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    /*La altura debe ser la justa para que entren los elementos en linea (row)*/
+    height:20%;
+  }
+
+  #content-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    /*La altura debe ser la justa para que entren los elementos en linea (row)*/
+    height:80%;
+  }
+
+  #results-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: aqua;
+    height: 100%;
+  }
+
+  #map-container {
+    display: flex;
   }
 
 
