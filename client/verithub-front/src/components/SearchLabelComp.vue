45542840ba580bfd566ebd2afff910791f4acd8c
AlvaroCasado99 <47740698+AlvaroCasado99@users.noreply.github.com>
2022-05-13 13:00:36 +0200

    Primer commit - vista serch result y componente serch label creados

---
+++ client/verithub-front/src/components/SearchLabelComp.vue
@@ -0,0 +1,21 @@
+<!-- SEARCH LABEL:
+Descripcion:
+Se trata de un elemento que se repetirá y que debe mostrar un texto u otro.
+
+Partes:
+    - Nombre de la universidad.
+    - Rating de la universidad.
+    - Distancia a la universidad.(?)
+-->
+
+<template>
+    <div id="initial">
+        
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ client/verithub-front/src/views/ResultsView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <HeaderComp />
+    <SearchBar />
+    <FooterComp />
+    <SearchLabelComp />
+    
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+import FooterComp from '../components/FooterComp.vue';
+import SearchBar from '../components/SearchBar.vue';
+import HeaderComp from '../components/HeaderComp.vue';
+import SearchLabelComp from '../components/SearchLabelComp.vue';
+
+export default defineComponent({
+  name: 'ResultsView',
+
+  components: {
+    FooterComp,
+    SearchBar,
+    HeaderComp,
+    SearchLabelComp,
+  },
+});
+</script>
+
+<style lang="css">
+
+  /* fuente Palatino Linotype */
+  @font-face {
+    font-family: 'Palatino Linotype';
+    font-style: normal;
+    font-weight: 400;
+    src: local('Palatino Linotype'), local('PalatinoLTStd-Roman'), url(https://fonts.gstatic.com/s/palatinolinotype/v9/lJwE-pIzkQ5ey4VjC7t6Gc7KZdw.woff2) format('woff2');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215, U+E0FF, U+EFFD, U+F000;
+  }
+
+  /* Todo el texto en esa fuente */
+  body {
+    font-family: 'Palatino Linotype', 'Times New Roman', serif;
+  }
+
+
+
+</style>
